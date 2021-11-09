651cac20ad7b6087f3a7d26b0dd3ccdc3ed37f68
H-mmer <hammer@ghodrag.fi>
2021-11-09 18:39:54 +0200

    updated

--- DCTF/Scooby-Doo/scooby-doo.sh
+++ DCTF/Scooby-Doo/scooby-doo.sh
@@ -2,13 +2,13 @@
 
 a="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-for i in {1..22}
+for y in {1..22}
 do
-s=$(cat cipher.txt | cut -b $j | tr -d '\n')
+s=`cat cipher.txt | cut -b $y | tr -d '\n'`
   for x in {0..25}
   do
-      cc=${a:$x:1}
-      if [[ $s == *"$c"* ]]
+      char=${a:$x:1}
+      if [[ $s == *"$char"* ]]
       then
           printf ''
       else
